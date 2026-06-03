81ee99b11635d917898a16f1b957a6abdb37ef6c
lukashunziker99 <122650838+lukashunziker99@users.noreply.github.com>
2026-06-03 13:05:20 +0200

    Create config_flow.py

---
+++ custom_components/solmate/config_flow.py
@@ -0,0 +1,21 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN, DEFAULT_PORT
+from .coordinator import SolMateCoordinator
+
+class SolMateConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+    async def async_step_user(self, user_input=None):
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title=f"SolMate {user_input['host']}",
+                data=user_input
+            )
+
+        schema = vol.Schema({
+            vol.Required("host"): str,
+            vol.Optional("port", default=DEFAULT_PORT): int,
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema)
