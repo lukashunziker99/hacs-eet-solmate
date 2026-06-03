6723c87726fe05f54dde2c5a77eb672a7959656f
lukashunziker99 <122650838+lukashunziker99@users.noreply.github.com>
2026-06-03 13:40:04 +0200

    Update _init_.py

--- custom_components/solmate/_init_.py
+++ custom_components/solmate/_init_.py
@@ -1,21 +1,26 @@
+from .const import DOMAIN
 from .coordinator import SolMateCoordinator
-
-DOMAIN = "solmate"
 
 async def async_setup_entry(hass, entry):
     host = entry.data["host"]
     port = entry.data["port"]
 
     coordinator = SolMateCoordinator(hass, host, port)
+
     await coordinator.start()
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, ["sensor", "number", "select", "switch"])
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        ["sensor", "number", "select", "switch"]
+    )
 
     return True
 
 
 async def async_unload_entry(hass, entry):
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    await coordinator.stop()
     return True
