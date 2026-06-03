42e4fad70b2c98c99e8d800531902561773acaaa
lukashunziker99 <122650838+lukashunziker99@users.noreply.github.com>
2026-06-03 13:41:36 +0200

    Update number.py

--- custom_components/solmate/number.py
+++ custom_components/solmate/number.py
@@ -1,13 +1,16 @@
 from homeassistant.components.number import NumberEntity
 
 async def async_setup_entry(hass, entry, async_add_entities):
+
     coordinator = hass.data["solmate"][entry.entry_id]
 
     async_add_entities([
         SolMateNumber(coordinator)
     ])
 
+
 class SolMateNumber(NumberEntity):
+
     def __init__(self, coordinator):
         self.coordinator = coordinator
         self._attr_name = "SolMate Battery Reserve"
@@ -20,5 +23,8 @@ class SolMateNumber(NumberEntity):
         return self.coordinator.data.get("battery_reserve")
 
     async def async_set_value(self, value: float):
-        # TODO: WRITE COMMAND (aus mmattel API einsetzen)
-        await self.coordinator._ws_loop()
+        if self.coordinator.writer:
+            await self.coordinator.writer.write(
+                "battery_reserve",
+                int(value)
+            )
