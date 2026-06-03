f08e0084600b519cd5877ddbb737723aed55f62d
lukashunziker99 <122650838+lukashunziker99@users.noreply.github.com>
2026-06-03 13:39:34 +0200

    Update const.py

--- custom_components/solmate/const.py
+++ custom_components/solmate/const.py
@@ -1,2 +1,13 @@
 DOMAIN = "solmate"
-DEFAULT_PORT = 80
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+
+DEFAULT_PORT = 8080
+
+SENSORS = {
+    "pv_power": "PV Power",
+    "battery_soc": "Battery SOC",
+    "grid_power": "Grid Power",
+    "consumption": "Consumption",
+}
