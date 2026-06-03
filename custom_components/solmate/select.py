e8126ddfc0f14052a4e342fc4a6e550e94125aac
lukashunziker99 <122650838+lukashunziker99@users.noreply.github.com>
2026-06-03 13:41:56 +0200

    Update select.py

--- custom_components/solmate/select.py
+++ custom_components/solmate/select.py
@@ -3,13 +3,14 @@ from homeassistant.components.select import SelectEntity
 OPTIONS = ["auto", "eco", "manual"]
 
 async def async_setup_entry(hass, entry, async_add_entities):
+
     coordinator = hass.data["solmate"][entry.entry_id]
 
-    async_add_entities([
-        SolMateSelect(coordinator)
-    ])
+    async_add_entities([SolMateSelect(coordinator)])
+
 
 class SolMateSelect(SelectEntity):
+
     def __init__(self, coordinator):
         self.coordinator = coordinator
         self._attr_name = "SolMate Mode"
@@ -24,5 +25,5 @@ class SolMateSelect(SelectEntity):
         return self.coordinator.data.get("mode")
 
     async def async_select_option(self, option):
-        # TODO WRITE COMMAND
-        pass
+        if self.coordinator.writer:
+            await self.coordinator.writer.write("mode", option)
