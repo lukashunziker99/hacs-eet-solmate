223bc6fb67cc94ac2716054d953daa77077d68ef
lukashunziker99 <122650838+lukashunziker99@users.noreply.github.com>
2026-06-03 14:06:50 +0200

    Update sensor.py

--- custom_components/solmate/sensor.py
+++ custom_components/solmate/sensor.py
@@ -4,8 +4,9 @@ from .entity import SolMateEntity
 class SolMateSensor(Entity, SolMateEntity):
 
     def __init__(self, coordinator, key, name):
-        SolMateEntity.__init__(self, coordinator)
+        self.coordinator = coordinator
         self.key = key
+
         self._attr_name = f"SolMate {name}"
         self._attr_unique_id = f"solmate_{key}"
 
